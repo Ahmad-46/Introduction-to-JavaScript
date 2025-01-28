8dd2b62064115a25b68c37e86e100b461744088f
Ahmad-46 <mubbaranauman@gmail.com>
2025-01-28 07:02:32 -0700

    java

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
 // this is javascript comment
-let number1 = 30;// javascript variable defining.
-let number2 = 20;
+let number1 = 100;// javascript variable defining.
+let number2 = 345;
 let result = number1 + number2;// creating another variable to store the summation of two numbers.
 let result2 =`the sum of ${number1} and ${number2} is ${result}.`//this variable combine a string and inside the string it contain three more variable.
 document.getElementById("results").innerHTML = result2;// this code is responsible for showing javascript result into the HTML <p> element(having the ID ="result") which will eventually show the summation result according to the variable 
 // note:each of the line of the javascript code is called statement
-let x = 5;
-let y = 2;
-let z = x * y;
